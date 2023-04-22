07eac5f75cff8eb56e879000b061075707956753
cybourgeoisie <bheidorn@neondistrict.io>
2023-04-22 09:51:17 -0400

    More environment setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("entrypoint");
+import { Configuration, OpenAIApi } from "openai";
+import cfg from "./config/config";
+
+(async () => {
+	const configuration = new Configuration({
+		apiKey: cfg.OPENAI_API_KEY,
+	  });
+	  const openai = new OpenAIApi(configuration);
+	  const response = await openai.listModels();
+
+	  console.log(response.data);
+})();
