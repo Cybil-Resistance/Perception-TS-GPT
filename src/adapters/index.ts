122a6fac97ee549218806ef3c39a43453094f11c
cybourgeoisie <bheidorn@neondistrict.io>
2023-05-01 14:22:08 -0400

    Remove the temp bot, which was moved into a separate repo

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,8 +1,7 @@
 import AutoBotAdapter from "./autobot/AutoBotAdapter";
 import ChatGPTBotAdapter from "./chatgptbot/ChatGPTBotAdapter";
 import CodeBotAdapter from "./codebot/CodeBotAdapter";
-import GenesisMechBotAdapter from "./genesismechbot/GenesisMechBotAdapter";
 import PerceptionBotAdapter from "./perceptionbot/PerceptionBotAdapter";
 import SummaryBotAdapter from "./summarybot/SummaryBotAdapter";
 
-export const Adapters = [AutoBotAdapter, ChatGPTBotAdapter, CodeBotAdapter, GenesisMechBotAdapter, PerceptionBotAdapter, SummaryBotAdapter];
+export const Adapters = [AutoBotAdapter, ChatGPTBotAdapter, CodeBotAdapter, PerceptionBotAdapter, SummaryBotAdapter];
