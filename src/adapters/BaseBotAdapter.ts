088942dcddca886a80b820236e09d8d31580b8fc
cybourgeoisie <bheidorn@neondistrict.io>
2023-05-07 08:15:26 -0400

    Refactor, expose protected configPath, getConfig, setConfig via Base Adapter

--- src/adapters/BaseBotAdapter.ts
+++ src/adapters/BaseBotAdapter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 
 export type AdapterRoute = {
 	endpoint: string;
@@ -7,6 +8,8 @@ export type AdapterRoute = {
 };
 
 export class BaseBotAdapter {
+	protected static configPath: string = process.cwd() + "/data/config.json";
+
 	public static getName(): string {
 		throw new Error("getName not implemented.");
 	}
@@ -21,5 +24,39 @@ export class BaseBotAdapter {
 
 	public static async run(): Promise<void> {
 		throw new Error("run not implemented.");
+	}
+
+	protected static getConfig(key: string): string {
+		// Determine if the config file exists in the data directory
+		if (!fs.existsSync(this.configPath)) {
+			// Create if not
+			fs.writeFileSync(this.configPath, "{}");
+		}
+
+		// Read the config file
+		const config: string = fs.readFileSync(this.configPath, "utf8");
+
+		// Parse the config file and return the value for the key, or null if not
+		return JSON.parse(config)[key] || null;
+	}
+
+	protected static setConfig(key: string, value: string): void {
+		// Determine if the config file exists in the data directory
+		if (!fs.existsSync(this.configPath)) {
+			// Create if not
+			fs.writeFileSync(this.configPath, "{}");
+		}
+
+		// Read the config file
+		const config: string = fs.readFileSync(this.configPath, "utf8");
+
+		// Parse the config file
+		const configJson: object = JSON.parse(config);
+
+		// Set the value for the key
+		configJson[key] = value;
+
+		// Write the config file
+		fs.writeFileSync(this.configPath, JSON.stringify(configJson, null, 2));
 	}
 }
--- src/adapters/codebot/CodeBotAdapter.ts
+++ src/adapters/codebot/CodeBotAdapter.ts
@@ -6,7 +6,7 @@ import Git from "@src/operations/git";
 import { BaseBotAdapter } from "@src/adapters/BaseBotAdapter";
 
 export default class PerceptionBotAdapter extends BaseBotAdapter {
-	private static configPath: string = process.cwd() + "/data/codebot.json";
+	protected static configPath: string = process.cwd() + "/data/codebot.json";
 	public static homeDirectory: string;
 	public static repositoryDirectory: string;
 
@@ -30,40 +30,6 @@ export default class PerceptionBotAdapter extends BaseBotAdapter {
 			// Enter the main menu
 			this.mainMenu();
 		}
-	}
-
-	private static getConfig(key: string): string {
-		// Determine if the config file exists in the data directory
-		if (!fs.existsSync(this.configPath)) {
-			// Create if not
-			fs.writeFileSync(this.configPath, "{}");
-		}
-
-		// Read the config file
-		const config: string = fs.readFileSync(this.configPath, "utf8");
-
-		// Parse the config file and return the value for the key, or null if not
-		return JSON.parse(config)[key] || null;
-	}
-
-	private static setConfig(key: string, value: string): void {
-		// Determine if the config file exists in the data directory
-		if (!fs.existsSync(this.configPath)) {
-			// Create if not
-			fs.writeFileSync(this.configPath, "{}");
-		}
-
-		// Read the config file
-		const config: string = fs.readFileSync(this.configPath, "utf8");
-
-		// Parse the config file
-		const configJson: object = JSON.parse(config);
-
-		// Set the value for the key
-		configJson[key] = value;
-
-		// Write the config file
-		fs.writeFileSync(this.configPath, JSON.stringify(configJson, null, 2));
 	}
 
 	public static async mainMenu(): Promise<void> {
